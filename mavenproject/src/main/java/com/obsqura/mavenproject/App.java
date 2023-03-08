5df0412ce230998776be5b5e3645fe6b3f426bc2
KPS <KPS@10.1.0.241>
2023-03-08 22:39:25 +0530

    2ND COMMIT

---
+++ mavenproject/src/main/java/com/obsqura/mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.obsqura.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello JAVA " );
+    }
+}
